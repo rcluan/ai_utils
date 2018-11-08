474bc1a98b596678a3594af14c3c4d88556afe57
Luan Rios Campos <luan.rios.campos@gmail.com>
2018-11-08 14:24:08 -0300

    add numpy option to metrics

--- ai_utils/metrics/__init__.py
+++ ai_utils/metrics/__init__.py
@@ -5,14 +5,16 @@ name='metrics'
 
 # R^2
 # https://www.kaggle.com/c/mercedes-benz-greener-manufacturing/discussion/34019
-def R_squared(y_true, y_pred):
+def R_squared(y_true, y_pred, to_numpy=False):
   SS_res =  K.sum(K.square(y_true - y_pred)) 
   SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
-  return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+
+  R2 = ( 1 - SS_res/(SS_tot + K.epsilon()) )
+  return K.get_value(R2) if to_numpy else R2
 
 # Factor of two
 # https://link.springer.com/content/pdf/10.1007/s00703-003-0070-7.pdf
-def fac2(y_true, y_pred):
+def fac2(y_true, y_pred, to_numpy=False):
   min_ = 0.5
   max_ = 2
 
@@ -27,12 +29,14 @@ def fac2(y_true, y_pred):
   res = tf.equal(greater_min, less_max)
   res = tf.cast(res, tf.float32)
 
-  return tf.reduce_mean(res)
+  fac_2 = tf.reduce_mean(res)
+
+  return K.get_value(fac_2) if to_numpy else fac_2
 
 # Pearson r
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.pearsonr.html
 # https://stackoverflow.com/questions/46619869/how-to-specify-the-correlation-coefficient-as-the-loss-function-in-keras
-def pearson_r(y_true, y_pred):
+def pearson_r(y_true, y_pred, to_numpy=False):
     x = y_true
     y = y_pred
     mx = K.mean(x)
@@ -42,4 +46,4 @@ def pearson_r(y_true, y_pred):
     r_den = K.sqrt(tf.multiply(K.sum(K.square(xm)), K.sum(K.square(ym))))
     r = r_num / r_den
 
-    return r
+    return K.get_value(r) if to_numpy else r
