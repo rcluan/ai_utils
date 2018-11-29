4f6a594494328da3e1595e7536a8fa3f89b6bee1
Luan Rios Campos <luan.rios.campos@gmail.com>
2018-11-29 16:00:16 -0300

    add send mail

--- ai_utils/__init__.py
+++ ai_utils/__init__.py
@@ -2,7 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedKFold
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
 
 def load_dataset(filename):
 
@@ -25,16 +30,19 @@ def preprocess(dataset, reset_index=False, fill_na_columns=[], drop_columns=[], 
 
     dataset.drop(drop_columns, inplace=True, axis=axis)
 
-#https://www.kaggle.com/stefanie04736/simple-keras-model-with-k-fold-cross-validation
-#https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.StratifiedKFold.html
+# https://www.kaggle.com/stefanie04736/simple-keras-model-with-k-fold-cross-validation
+# https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.StratifiedKFold.html
+
+
 def k_fold(k, x_data, y_data, shuffle=True):
     return StratifiedKFold(n_splits=k, shuffle=shuffle).split(x_data, y_data)
 
-def plot_pred(y_true, y_pred, save_to="~/pred.png", labels=['x', 'y'], line_width = [1,1]):
-    
+
+def plot_pred(y_true, y_pred, save_to="~/pred.png", labels=['x', 'y'], line_width=[1, 1]):
+
     plt.plot(y_true, label='True', linewidth=line_width[0])
     plt.plot(y_pred, label='Prediction', linewidth=line_width[1])
-        
+
     # Plot labels etc.
     plt.xlabel(labels[0])
     plt.ylabel(labels[1])
@@ -42,3 +50,55 @@ def plot_pred(y_true, y_pred, save_to="~/pred.png", labels=['x', 'y'], line_widt
     #plt.xticks([i for i in range(0, len(processes), 250)], processes)
     plt.savefig(save_to)
     plt.show()
+
+
+"""
+    Example:
+    send_mail(
+        "example@gmail.com",
+        "123456",
+        "smtp.google.com:587",
+        "john@live.com",
+        "Report",
+        "Sending the report.",
+        [str(path + "report.pdf")],
+        "doe@gmail.com"
+    )
+"""
+
+def send_mail(username, password, smtphost, to, subject, msg_body, attachments=None, cc=None):
+    try:
+        msg = MIMEMultipart()
+        message = msg_body
+
+        msg['From'] = username
+        msg['To'] = to
+
+        if cc is not None:
+            msg['Cc'] = cc
+
+        msg['Subject'] = subject
+        msg.attach(MIMEText(message, 'plain'))
+
+        if attachments is not None:
+            for f in attachments:
+                f = os.path.expanduser(f)
+                print(f)
+                with open(f, "rb") as fil:
+                    part = MIMEApplication(
+                        fil.read(),
+                        Name=basename(f)
+                    )
+                # After the file is closed
+                part['Content-Disposition'] = 'attachment; filename="%s"' % basename(
+                    f)
+                msg.attach(part)
+
+        server = smtplib.SMTP(smtphost)
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(msg['From'], msg['To'], msg.as_string())
+        server.quit()
+        print("Successfully sent email to %s:" % (msg['To']))
+    except:
+        print("Error when sending email")
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ai_utils',
-      version='1.0.2',
+      version='1.0.4',
       description='Utilities to be used with Tensorflow and Keras for AI techniques',
       url='http://github.com/rcluan/ai_utils',
       author='Luan Campos',
@@ -12,6 +12,7 @@ setup(name='ai_utils',
           'tensorflow',
           'pandas',
           'matplotlib',
+          'numpy',
           'sklearn',
           'xlrd'
       ],
