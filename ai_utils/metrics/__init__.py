5cf7135f8bf2ad9b0265a36e96f3c46f6088c830
pedronovaes <pedronovaesmelo@gmail.com>
2018-12-14 14:17:06 -0300

    Adding functions to plot factor of two metric graphic

--- ai_utils/metrics/__init__.py
+++ ai_utils/metrics/__init__.py
@@ -1,37 +1,37 @@
 import tensorflow as tf
 from tensorflow.keras import backend as K
 
-name='metrics'
+name = 'metrics'
 
 # R^2
 # https://www.kaggle.com/c/mercedes-benz-greener-manufacturing/discussion/34019
 def R_squared(y_true, y_pred, to_numpy=False):
-  SS_res =  K.sum(K.square(y_true - y_pred)) 
-  SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
+    SS_res =  K.sum(K.square(y_true - y_pred))
+    SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
 
-  R2 = ( 1 - SS_res/(SS_tot + K.epsilon()) )
-  return K.get_value(R2) if to_numpy else R2
+    R2 = (1 - SS_res/(SS_tot + K.epsilon()))
+    return K.get_value(R2) if to_numpy else R2
 
 # Factor of two
 # https://link.springer.com/content/pdf/10.1007/s00703-003-0070-7.pdf
 def fac2(y_true, y_pred, to_numpy=False):
-  min_ = 0.5
-  max_ = 2
+    min_ = 0.5
+    max_ = 2
 
-  division = tf.divide(y_pred, y_true)
-  
-  greater_min = tf.greater_equal(division, min_)
-  less_max = tf.less_equal(division, max_)
+    division = tf.divide(y_pred, y_true)
 
-  #greater_min = tf.cast(greater_min, tf.float32)
-  #less_max = tf.cast(less_max, tf.float32)
+    greater_min = tf.greater_equal(division, min_)
+    less_max = tf.less_equal(division, max_)
 
-  res = tf.equal(greater_min, less_max)
-  res = tf.cast(res, tf.float32)
+    # greater_min = tf.cast(greater_min, tf.float32)
+    # less_max = tf.cast(less_max, tf.float32)
 
-  fac_2 = tf.reduce_mean(res)
+    res = tf.equal(greater_min, less_max)
+    res = tf.cast(res, tf.float32)
 
-  return K.get_value(fac_2) if to_numpy else fac_2
+    fac_2 = tf.reduce_mean(res)
+
+    return K.get_value(fac_2) if to_numpy else fac_2
 
 # Pearson r
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.pearsonr.html
